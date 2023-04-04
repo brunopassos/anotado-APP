e9e99030b48332cf3c0aa1a3917f4326c9d76643
Bruno Passos <brunopassos_bp@hotmail.com>
2023-04-04 16:25:58 -0300

    feat: login page implemented

---
+++ src/components/ErrorText/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components/native";
+
+export const TextError = styled.Text`
+    font-size: 12px;
+    color: "rgba(200,0,50,1)";
+    text-align: center;
+    margin: 5px;
+`
