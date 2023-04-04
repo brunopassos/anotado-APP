f61916116a3b696a5d3364dc7cfce4cd9abe9b55
Bruno Passos <brunopassos_bp@hotmail.com>
2023-04-04 11:08:53 -0300

    initial libs

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from 'react-native';
+
+import { 
+  Container
+} from "./styles";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>ANOTADO</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <Text>Anotado</Text>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
